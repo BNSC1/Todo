f704509d187f8282fb8f0324a54721787a09810f
B.N <19542357+BNSC1@users.noreply.github.com>
2022-10-24 15:45:05 +0800

    - Update: error handling rearrange

--- app/src/main/java/com/bn/todo/arch/BaseViewModel.kt
+++ app/src/main/java/com/bn/todo/arch/BaseViewModel.kt
@@ -2,34 +2,18 @@ package com.bn.todo.arch
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.bn.todo.data.Resource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
-import kotlin.experimental.ExperimentalTypeInference
 
 abstract class BaseViewModel : ViewModel() {
     protected var job: Job? = null
-    protected val _errorMsg = MutableStateFlow("")
-    val errorMsg = _errorMsg.asStateFlow()
+    protected val _errorMsg = MutableSharedFlow<String>(replay = 0)
+    val errorMsg = _errorMsg.asSharedFlow()
 
-    @OptIn(ExperimentalTypeInference::class)
-    inline fun <T> ViewModel.stateFlow(
-        scope: CoroutineScope = viewModelScope,
-        started: SharingStarted = SharingStarted.Lazily,
-        initValue: Resource<T> = Resource.loading(),
-        @BuilderInference crossinline action: suspend FlowCollector<Resource<T>>.() -> Unit,
-    ): StateFlow<Resource<T>> =
-        flow {
-            action()
-        }.stateIn(
-            scope = scope,
-            started = started,
-            initialValue = initValue
-        )
-
-    protected fun tryLaunchAction(
+    protected fun tryRun(
         scope: CoroutineScope = viewModelScope,
         failureAction: suspend (Throwable) -> Unit = {},
         action: suspend () -> Unit
@@ -37,7 +21,7 @@ abstract class BaseViewModel : ViewModel() {
         runCatching {
             action()
         }.onFailure {
-            _errorMsg.value = it.message.toString()
+            _errorMsg.emit(it.message.toString())
             failureAction(it)
         }
     }
