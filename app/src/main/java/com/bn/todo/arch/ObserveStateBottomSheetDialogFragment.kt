7d93cf050d4cee11e55e724c3facd99e3f3c8d0f
B.N <19542357+BNSC1@users.noreply.github.com>
2022-10-26 14:08:12 +0800

    - Update: rename ErrorMessage to Message

--- app/src/main/java/com/bn/todo/arch/ObserveStateBottomSheetDialogFragment.kt
+++ app/src/main/java/com/bn/todo/arch/ObserveStateBottomSheetDialogFragment.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import androidx.viewbinding.ViewBinding
 import com.bn.todo.R
-import com.bn.todo.data.Resource
-import com.bn.todo.data.State
 import com.bn.todo.ktx.collectLifecycleFlow
 import com.bn.todo.ktx.showDialog
 import com.bn.todo.ktx.showToast
 import kotlinx.coroutines.launch
-import timber.log.Timber
 
 abstract class ObserveStateBottomSheetDialogFragment<Binding : ViewBinding> :
     BaseBottomSheetDialogFragment<Binding>() {
@@ -25,11 +21,11 @@ abstract class ObserveStateBottomSheetDialogFragment<Binding : ViewBinding> :
         savedInstanceState: Bundle?
     ): View {
         initViewBinding(container)
-        observeErrorMsg()
+        collectMessage()
         return binding.root
     }
 
-    private fun observeErrorMsg() {
+    private fun collectMessage() {
         job = viewLifecycleOwner.lifecycleScope.launch {
             viewModel.message.collectLifecycleFlow(viewLifecycleOwner) {
                 when (it) {
@@ -38,25 +34,6 @@ abstract class ObserveStateBottomSheetDialogFragment<Binding : ViewBinding> :
                         showToast(getString(R.string.msg_deleted_todos_format).format(it.count))
                 }
             }
-        }
-    }
-
-
-    fun handleState(
-        resource: Resource<*>,
-        successAction: () -> Unit,
-        errorAction: () -> Unit = {},
-        loadingAction: () -> Unit = {},
-    ) {
-        Timber.d("state is ${resource.state}")
-        when (resource.state) {
-            State.SUCCESS -> successAction()
-            State.ERROR -> {
-                errorAction()
-                showToast(resource.message ?: resource.messageResId?.let { getString(it) }
-                ?: getString(R.string.err_unknown), Toast.LENGTH_LONG)
-            }
-            State.LOADING -> loadingAction()
         }
     }
 }
--- app/src/main/java/com/bn/todo/arch/ObserveStateFragment.kt
+++ app/src/main/java/com/bn/todo/arch/ObserveStateFragment.kt
@@ -6,12 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.viewbinding.ViewBinding
 import com.bn.todo.R
-import com.bn.todo.data.Resource
-import com.bn.todo.data.State
 import com.bn.todo.ktx.collectLatestLifecycleFlow
 import com.bn.todo.ktx.showDialog
 import com.bn.todo.ktx.showToast
-import timber.log.Timber
 
 abstract class ObserveStateFragment<Binding : ViewBinding> : BaseFragment<Binding>() {
     protected abstract val viewModel: BaseViewModel
@@ -21,34 +18,17 @@ abstract class ObserveStateFragment<Binding : ViewBinding> : BaseFragment<Bindin
         savedInstanceState: Bundle?
     ): View {
         initViewBinding(container)
-        collectErrorMessage()
+        collectMessage()
         return binding.root
     }
 
-    private fun collectErrorMessage() {
+    private fun collectMessage() {
         viewModel.message.collectLatestLifecycleFlow(viewLifecycleOwner) {
             when (it) {
                 is ViewModelMessage.Error -> showDialog(message = it.msg)
                 is ViewModelMessage.Info.CompletedTodoDeletion ->
                     showToast(getString(R.string.msg_deleted_todos_format).format(it.count))
             }
-        }
-    }
-
-
-    fun handleState(
-        resource: Resource<*>,
-        errorAction: () -> Unit = {},
-        loadingAction: () -> Unit = {},
-        successAction: () -> Unit
-    ) {
-        Timber.d("state is ${resource.state}")
-        when (resource.state) {
-            State.SUCCESS -> successAction()
-            State.ERROR -> {
-                errorAction()
-            }
-            State.LOADING -> loadingAction()
         }
     }
 }
