155aa1884d3e0c24eb995a7db21444bd45c029ca
B.N <19542357+BNSC1@users.noreply.github.com>
2022-11-01 16:23:27 +0800

    - Update: base classes code cleanup

--- app/src/main/java/com/bn/todo/arch/BaseViewModel.kt
+++ app/src/main/java/com/bn/todo/arch/BaseViewModel.kt
@@ -3,13 +3,11 @@ package com.bn.todo.arch
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 abstract class BaseViewModel : ViewModel() {
-    protected var job: Job? = null
     protected val _message = MutableSharedFlow<ViewModelMessage>(replay = 0)
     val message = _message.asSharedFlow()
 
