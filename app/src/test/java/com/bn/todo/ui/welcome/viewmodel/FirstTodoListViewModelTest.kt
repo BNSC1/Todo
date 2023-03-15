d3d58b782b09f3ceaa9092a457b42cc9e502fa1f
Tim Wang <timwang@moneydj.com>
2023-03-15 11:19:56 +0800

    - Update: alarm permission - Update: first todo list view model unit test

--- app/src/test/java/com/bn/todo/ui/welcome/viewmodel/FirstTodoListViewModelTest.kt
+++ app/src/test/java/com/bn/todo/ui/welcome/viewmodel/FirstTodoListViewModelTest.kt
@@ -5,6 +5,7 @@ import com.bn.todo.MainCoroutineExtension
 import com.bn.todo.data.repository.MockTodoRepository
 import com.bn.todo.data.repository.MockUserPrefRepository
 import com.bn.todo.usecase.InsertTodoListUseCase
+import com.bn.todo.usecase.SetCurrentListIdUseCase
 import com.bn.todo.usecase.SetIsNotFirstLaunchUseCase
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -20,6 +21,7 @@ internal class FirstTodoListViewModelTest {
     private lateinit var viewModel: FirstTodoListViewModel
     private lateinit var insertTodoListUseCase: InsertTodoListUseCase
     private lateinit var setIsNotFirstLaunchUseCase: SetIsNotFirstLaunchUseCase
+    private lateinit var setCurrentListIdUseCase: SetCurrentListIdUseCase
     private val todoRepository = MockTodoRepository()
     private val userPrefRepository = MockUserPrefRepository()
     private val todoListName = "todoListName"
@@ -28,14 +30,34 @@ internal class FirstTodoListViewModelTest {
     fun setup() {
         insertTodoListUseCase = InsertTodoListUseCase(todoRepository)
         setIsNotFirstLaunchUseCase = SetIsNotFirstLaunchUseCase(userPrefRepository)
-        viewModel = FirstTodoListViewModel(insertTodoListUseCase, setIsNotFirstLaunchUseCase)
+        setCurrentListIdUseCase = SetCurrentListIdUseCase(userPrefRepository)
+        viewModel = FirstTodoListViewModel(
+            insertTodoListUseCase,
+            setIsNotFirstLaunchUseCase,
+            setCurrentListIdUseCase
+        )
     }
 
     @Test
-    fun `add first todo list`() = runTest {
+    fun `add first todo list no input`() = runTest {
         viewModel.insertTodoList(todoListName).join()
 
         todoRepository.todoLists shouldHaveSize 1
+        todoRepository.todoLists.first().name `should be equal to` todoListName
+        userPrefRepository.getIsFirstTimeLaunch().test {
+            awaitItem() `should be equal to` false
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `add first todo list has input`() = runTest {
+        val inputName = "inputName"
+        viewModel.setInputName(inputName)
+        viewModel.insertTodoList(todoListName).join()
+
+        todoRepository.todoLists shouldHaveSize 1
+        todoRepository.todoLists.first().name `should be equal to` inputName
         userPrefRepository.getIsFirstTimeLaunch().test {
             awaitItem() `should be equal to` false
             cancelAndIgnoreRemainingEvents()
